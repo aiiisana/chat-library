63ff06242dfb9ce348daf7f0fa26dfef79749f0d
aiiisana <aisanaond@gmail.com>
2025-04-09 18:30:11 +0500

    updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
+import java.util.Properties
+
+val githubProperties = Properties().apply {
+    val githubPropertiesFile = rootProject.file("github.properties")
+    if (githubPropertiesFile.exists()) {
+        load(githubPropertiesFile.inputStream())
+    }
+}
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    `maven-publish`
 }
 
 android {
@@ -51,4 +61,29 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.aiiisana"
+                artifactId = "chatlibrary"
+                version = "1.0.0"
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/aiiisana/chat-library")
+                credentials {
+                    username = githubProperties["gpr.usr"] as String? ?: System.getenv("GPR_USER")
+                    password = githubProperties["gpr.key"] as String? ?: System.getenv("GPR_API_KEY")
+                }
+            }
+        }
+    }
 }
