6472e72d6272401437daaafaf6ba071aba9b6c6f
aiiisana <aisanaond@gmail.com>
2025-04-30 21:07:46 +0500

    Release v1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,8 @@
 import java.util.Properties
 
-val githubProperties = Properties().apply {
-    val githubPropertiesFile = rootProject.file("github.properties")
-    if (githubPropertiesFile.exists()) {
-        load(githubPropertiesFile.inputStream())
-    }
-}
-
 plugins {
-    id("com.android.library") version "8.5.1"
-    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
 
@@ -51,37 +44,25 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-
-    // WebSocket
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-
-    // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-
-    // Koin
     implementation("io.insert-koin:koin-android:3.4.0")
-
-    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "com.github.aiiisana"
-                artifactId = "chat-library"
-                version = "1.1.1"
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.aiiisana"
+            artifactId = "chat-library"
+            version = "1.1.1"
 
-                afterEvaluate {
-                    from(components["release"])
-                }
+            afterEvaluate {
+                from(components["release"])
             }
         }
     }
